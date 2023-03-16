6dd7fce0e14444631d741806d45643e71870c9a8
Pitchaya Inpibul <6310682627@student.tu.ac.th>
2023-03-17 01:06:53 +0700

    Edit Score_increase

--- MobileQuizGame/app/src/main/java/com/example/mobilequizgame/data/QuestionsData.kt
+++ MobileQuizGame/app/src/main/java/com/example/mobilequizgame/data/QuestionsData.kt
@@ -1,7 +1,7 @@
 package com.example.mobilequizgame.data
 
 const val MAX_NO_OF_QUESTIONS = 10
-const val SCORE_INCREASE = 20
+const val SCORE_INCREASE = 1
 
 val allQuestions = listOf(
     Question("Question 1 Here and Correct Answer is choice 1", listOf("choice 1 here", "choice 2 here", "choice 3 here", "choice 4 here"), "choice 1 here"),
